4865f264b0695e7fe2211044180603c903259a7f
priyangasaravan-cyber <priyangasaravan@gmail.com>
2026-03-24 09:05:56 +0530

    UC2: Add Passenger Bogies to Train (ArrayList Operations)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,22 +1,33 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
-        System.out.println("========================================");
-        System.out.println("=== Train Consist Management App ===");
-        System.out.println("========================================\n");
+        ArrayList<String> passengerBogies = new ArrayList<>();
 
-        List<String> trainConsist = new ArrayList<>();
+        System.out.println("=====================================");
+        System.out.println(" UC2 - Add Passenger Bogies to Train ");
+        System.out.println("=====================================");
 
-        System.out.println("Train initialized successfully...");
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
 
-        System.out.println("Initial Bogie Count : " + trainConsist.size());
+        System.out.println("After Adding Bogies:");
+        System.out.println("Passenger Bogies: " + passengerBogies + "\n");
 
-        System.out.println("Current Train Consist : " + trainConsist);
+        passengerBogies.remove("AC Chair");
 
-        System.out.println("\nSystem ready for operations...");
+        System.out.println("After Removing 'AC Chair':");
+        System.out.println("Passenger Bogies: " + passengerBogies + "\n");
+
+        System.out.println("Checking if 'Sleeper' exists:");
+        System.out.println("Contains 'Sleeper'? " + passengerBogies.contains("Sleeper") + "\n");
+
+        System.out.println("Final Train Passenger Consist:");
+        System.out.println(passengerBogies + "\n");
+
+        System.out.println("All passenger operations completed successfully.");
     }
 }
