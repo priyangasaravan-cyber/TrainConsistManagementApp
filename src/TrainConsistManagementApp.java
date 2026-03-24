8a69cd3329b00548aebc70565bb04ee1747cdc8b
priyangasaravan-cyber <priyangasaravan@gmail.com>
2026-03-24 09:19:43 +0530

    UC3: Track Unique Bogie IDs (Set – HashSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,33 +1,29 @@
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
+        System.out.println("---------------------------");
+        System.out.println(" UC3 Track Unique Bogie IDs ");
+        System.out.println("---------------------------");
 
-        ArrayList<String> passengerBogies = new ArrayList<>();
+        Set<String> bogies = new HashSet<>();
 
-        System.out.println("=====================================");
-        System.out.println(" UC2 - Add Passenger Bogies to Train ");
-        System.out.println("=====================================");
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
 
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
+        bogies.add("BG101");
+        bogies.add("BG102");
 
-        System.out.println("After Adding Bogies:");
-        System.out.println("Passenger Bogies: " + passengerBogies + "\n");
+        System.out.println("Bogie IDs After Insertion:");
+        System.out.println(bogies);
 
-        passengerBogies.remove("AC Chair");
+        System.out.println("\nNote:");
 
-        System.out.println("After Removing 'AC Chair':");
-        System.out.println("Passenger Bogies: " + passengerBogies + "\n");
-
-        System.out.println("Checking if 'Sleeper' exists:");
-        System.out.println("Contains 'Sleeper'? " + passengerBogies.contains("Sleeper") + "\n");
-
-        System.out.println("Final Train Passenger Consist:");
-        System.out.println(passengerBogies + "\n");
-
-        System.out.println("All passenger operations completed successfully.");
+        System.out.println("Duplicates are automatically ignored by HashSet.");
+        System.out.println("UC3 uniqueness validation completed...");
     }
 }
